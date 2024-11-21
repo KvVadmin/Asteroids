d2892a0ec1536635cb7768c3044c2c223359a538
KvVadmin <v.+1kornukh@gmail.com>
2024-11-21 23:36:09 +0100

    Added asteroids, asteroid field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -12,10 +14,17 @@ def main():
     # creating groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    #assign containers
+    asteroids = pygame.sprite.Group()
+    
+    #assign containers and definining class
     Player.containers = (updatable, drawable)
-    #create player after assigning containers
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    
+    #create an instance of Player class after assigning containers
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroid_field = AsteroidField()
     dt = 0
     
 
@@ -25,7 +34,7 @@ def main():
 
   
 
-    
+    #gameloop
     while True:
         
         print(f"dt: {dt}")  # get the time passed and update everything based on that new time (below methods)
@@ -42,7 +51,8 @@ def main():
 
         for obj in drawable:
             obj.draw(screen)
-            
+
+
         pygame.display.flip()
         
         dt = clock.tick(60)/1000
